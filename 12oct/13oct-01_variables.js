388afcc1b8373b56f9d2de87309739fcf072b459
krishna23tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-10-13 11:03:19 +0000

    variables

---
+++ 12oct/13oct-01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1233344
+let accounEmail = "kk@gmail.cpm"
+var accountPassword = "123456"
+let accountState
+
+accounrCity = "jaipur"
+
+// accountId = 123
+
+accounEmail = "ra@gmail.com"
+accountPassword = "098833"
+accounrCity = "banaras"
+
+
+console.log(accountId);
+console.log(accounEmail);
+console.log(accountPassword);
+
+console.table([accountId, accounEmail, accountPassword, accounrCity, accountState])
+
+
+
