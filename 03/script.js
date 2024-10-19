48084833a6ade9386ca462186dff5a920c28e1a1
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-10-19 14:01:16 +0530

    Add files via upload

---
+++ 03/script.js
@@ -0,0 +1,30 @@
+let x = "my name is krishna "
+let y = "i am a student"
+
+console.log(x + y)
+
+let age = 25
+
+let ishappy = true
+
+let z = "hi " + x +"and "+ y
+
+let a = "krishna"
+let b = "tiwari"
+
+let userinfo = 'hi my name is ' + a + " "+ b + " and i am " + age + " years old."
+
+let c = undefined
+
+
+{ let m = 'kk' }
+
+{ var n = 'kkt' }
+
+
+var k = "kk"
+
+let l = "kkt"
+
+console.log(k);
+console.log(l);
