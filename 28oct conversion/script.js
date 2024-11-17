54d46c7e080aa4b1c5fc81b5d859e41158e07d6f
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-11-17 10:37:13 +0530

    Add files via upload

---
+++ 28oct conversion/script.js
@@ -0,0 +1,29 @@
+// a = 10;
+
+// console.log(typeof a)
+
+// a = "10";
+// console.log(typeof(a));
+
+// a = "123";
+
+// b = Number(a)
+// console.log(typeof(b))
+
+// let l = 1;
+// let boo = Boolean(l)
+// console.log(boo);
+
+// let k = 0;
+// let bool = Boolean(k)
+// console.log(bool);
+
+// let x = "kk"
+// let xx = Boolean(x)
+// console.log(xx);
+
+// let x1 = ""
+// let xx1 = Boolean(x1)
+// console.log(xx1);
+
+console.log("2" == 2);
