94dee79181275f2a542d20a26cb4bb7cd0f6cb6f
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-11-17 10:38:18 +0530

    Add files via upload

---
+++ 29oct string/script.js
@@ -0,0 +1,36 @@
+const a = "kk"
+const b = "Tiwari"
+
+// console.log(a + b + "20");
+
+// console.log(`hello my name is ${a.toUpperCase()} and sirname is ${b.toLowerCase()}`);
+
+const abc = new String ("kk-tiwari")
+
+// console.log(abc[2]);
+
+// console.log(abc.length);
+
+// console.log(abc);
+// console.log(abc.toUpperCase());
+// console.log(abc.charAt(4));
+
+// console.log(abc.indexOf('w'));
+
+// const x1 = abc.substring(0,2)
+// console.log(x1);
+
+// const x2 = abc.slice(-8,3)
+// console.log(x2);
+
+// const x3 = "    kk   "
+// console.log(x3);
+// console.log(x3.trim());
+
+// const x4 = "kktiwari#gmail.com"
+// console.log(x4.replace('#', '@'));
+
+// console.log(x4.includes('tw'));
+
+const x5 = 'krishna-23-tiwari'
+console.log(x5.split('-'));
