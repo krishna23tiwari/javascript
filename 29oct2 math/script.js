815ac5beea0e62b1560758441d8cdb66b182329b
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-11-18 12:07:21 +0530

    Add files via upload

---
+++ 29oct2 math/script.js
@@ -0,0 +1,44 @@
+// const score = 1235
+
+// const x1 = new Number(200)
+// console.log(x1);
+
+// console.log(x1.toString().length);\
+
+// const x2 = new Number (267)
+// console.log(x2.toFixed(5));
+
+// const x3 = 234.999
+
+// console.log(x3.toPrecision(3));
+
+//it gives priority before decimal value
+
+// const x4 = 100000
+// console.log(x4.toLocaleString('en-IN'));
+
+// console.log(Math.abs(-44));
+
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.9));
+
+// console.log(Math.floor(4.9));
+
+// console.log(Math.sqrt(82));
+
+// console.log(Math.pow(2,3));
+
+// console.log(Math.min(4,9,12,44,6));
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10) + 1);
+
+const min = 10
+const max  = 20
+
+console.log((Math.floor(Math.random() * (max - min + 1))  + min) )
+
+
+
