fd6ffae889dbf223727c355ea991acd5ed21625d
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-11-20 11:43:44 +0530

    Add files via upload

---
+++ 29oct5 array2/script.js
@@ -0,0 +1,36 @@
+// const arr = [12,24,33,11,55]
+
+const arr1 = ["kk", "kkt", "jatin", "ravi"]
+const arr2 = ["milind", "kapil", "dev", "bavaa"]
+
+// arr1.push(arr2)
+
+// console.log(arr1);
+// console.log(arr1[4]);
+// console.log(arr1[4][1]);
+
+// const a = arr1.concat(arr2)
+
+// console.log(a);
+
+// const all = [...arr1, ...arr2]
+
+// console.log(all);
+
+// const another = [1,2,3, [44,23,11], 12, 16, [9,33,[43,666]]]
+
+// const r = another.flat(Infinity)
+// console.log(r);
+
+// console.log(Array.isArray("kkt"))
+
+// console.log(Array.from("kkt"))
+
+console.log(Array.from({name: "kkt"}))
+
+let a = 122
+let b = 244
+let c = 12344
+
+console.log(Array.of(a,b,c));
+
