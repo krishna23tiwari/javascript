0deee8a8f662e9d73ca0931052b07d0e158ee20e
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-12-05 15:33:58 +0530

    Add files via upload

---
+++ 30oct object3/script.js
@@ -0,0 +1,35 @@
+const x1 = {
+  id: 122,
+  name: "kk",
+  email: "kk@gmail.com"
+}
+
+// console.log(x1.id);
+
+// here we are trying to get value from object so for that we have to do x1.(key name) again and again
+// but here we can also use { here we write name which we have to access} = object name
+// and if we want to change name then like here we use email to eml {email: eml} so we can short that name
+// its called destructuring 
+
+const {email: eml} = x1
+
+// console.log(eml);
+
+// const navabar = ({company}) =>{
+// }
+
+// navabar(company =  "kk")
+
+//=========================API=============================================
+//api file comes in json
+//we can say its kind of object but not object because we declere name of the object but in json
+// keys and values both are in string format
+
+
+
+
+
+
+
+
+
