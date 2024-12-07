b96991762ba99cdb10a47cab82974e65a98f9c3c
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-12-07 19:45:30 +0530

    Add files via upload

---
+++ 30oct3 destructure_object3/script.js
@@ -0,0 +1,28 @@
+const x1 = {
+  id: 122,
+  name: "kk",
+  email: "kk@gmail.com"
+}
+
+// console.log(x1.id);
+
+// here we are trying to get value from object so for that we have to do x1.(key name) again and again
+// but here we can also use { here we write name which we have to access} = object name
+// and if we want to change name then like here we use email to eml {email: eml} so we can short that name
+// its called destructuring 
+
+const {email: eml} = x1
+
+// console.log(eml);
+
+// const navabar = ({company}) =>{
+// }
+
+// navabar(company =  "kk")
+
+=========================API=============================================
+
+
+
+
+
