c7aa3549c54012d3c0dcbdd4ad576b6e3cae987c
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-12-08 15:59:38 +0530

    Add files via upload

---
+++ 2nov callstack/script.js
@@ -0,0 +1,18 @@
+// code execution in js
+
+// execution context
+
+// when  we try to run js file in browser then first it created global execution context
+
+// when we run code in browser then global execution context for browser is diffrent and 
+// for not its also diffrent
+// so execution context for each other is diifrent
+
+// it works on thred and js is also single threaded language
+
+// there are mailny 2 execution context
+// 1. Global execution context
+// 2. function execution context
+// but there is third execution context also which is
+// 3. eval execution context
+
