5f1bae3f2cf9870784d6ce96df49d4dc6d740034
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-12-31 10:52:21 +0530

    Add files via upload

---
+++ 04/script.js
@@ -0,0 +1,13 @@
+
+// alert("Your message goes here")
+// const isconfirmed =  confirm("would you like to continue")
+function abc(){
+    console.log("hello");
+}
+
+abc();
+
+
+const a = 5
+console.log(a)
+
