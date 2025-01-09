735b203ccefe9649d3bdd166708ae46eb337bd19
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-01-09 19:16:07 +0530

    Add files via upload

---
+++ 5nov while-do-while-loop/script.js
@@ -0,0 +1,23 @@
+// let a = 0
+
+// while(a <= 10){
+//     console.log(a);
+//     a += 2 
+// }
+
+// let arr = ["hello", "hi", "kk"]
+
+// let b = 0
+// while (b < arr.length) {
+//     console.log(arr[b]);
+//     b++; 
+// }
+
+let k = 11
+
+do {
+    console.log(k);
+    k++
+
+} while (k <=10);
+
