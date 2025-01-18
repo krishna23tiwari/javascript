700d17b3a34cde9a726ddb29ae5c21fe5d3cf33e
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-01-18 20:55:56 +0530

    Add files via upload

---
+++ 07oct/script.js
@@ -0,0 +1,25 @@
+
+// alert("Your message goes here")
+// const isconfirmed =  confirm("would you like to continue")
+let obj = {
+    name1: "kk",
+    age: 26,
+    address: "jaipur",
+    obj1: {
+        a: "kk",
+        obj2: {
+            b: "kkt",
+            c: "kasda",
+            d: "afasf",
+        }
+        
+    }
+};
+
+
+ console.log(obj.obj1.obj2.b, obj.obj1.obj2.c);
+
+
+  
+//   console.log(obj.obj1);
+  
