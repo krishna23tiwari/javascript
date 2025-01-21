38864201ffe0536e030c5e6678e954221bdd3302
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-01-21 18:55:15 +0530

    Add files via upload

---
+++ 10nov1 filter-foreach/script.js
@@ -0,0 +1,50 @@
+// const a = ["js", "ruby", "python", "c", "c++", "java", "node"]
+
+// const b = a.forEach((n) =>{
+//     console.log(n);
+// })
+
+// console.log(b);
+
+// const a  = [12,44,55,33,23,52,12,51,66]
+
+// const b = a.filter((n) => n > 50 )
+
+// console.log(b);
+
+// const n  = [12,44,55,33,23,52,12,51,66]
+
+// const m = []
+
+// n.forEach((num) =>{
+//     if(num > 50){
+//         m.push(num)
+//     }
+// })
+
+// console.log(m);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+//   const a = books.filter((n) => n.genre === 'History')
+
+//   const b = books.filter((num) => num.publish >= 2000)
+
+//   const c = books.filter((num) => num.publish >= 1995 && num.genre === "History")
+
+//   console.log(a);
+
+//   console.log(b);
+
+//   console.log(c);
+
