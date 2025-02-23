088021fe9d84b3de9a78cad109350ef1bc0f15da
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-02-23 20:31:53 +0530

    Add files via upload

---
+++ 13dec-callback-callbackhell/script.js
@@ -0,0 +1,65 @@
+// function print(a){
+//     console.log(a)
+// }
+
+// function add(a,b, print){
+//     let s =  a+b;
+//     print(s)
+// }
+
+// add(5,5,print)
+
+// function x(y){
+//     console.log("first")
+// }
+
+// x(function y(){
+//     console.log("second")
+// })
+
+
+// function a(b){
+//     console.log("hello sir");
+//     b()
+// }
+
+// // function b(){
+// //     console.log("kk");
+// // }
+
+// a(function() {
+//     console.log('tiwari')
+// })
+
+// function b(){
+//     setTimeout(() =>{
+//         console.log("krishna")
+//     },4000)
+//     a()
+// }
+
+
+// function a(){
+//     console.log("hello")
+// }
+
+// b(a)
+// console.log("tiwari")
+
+// function sub(add){
+//     setInterval(() => {
+//         for(let i = 0; i<50; i++){
+//             console.log(add + i);
+//         }
+        
+//     }, 10000);
+// }
+
+
+// function add(a,b){
+//     return a + b;
+// }
+
+// sub(add(4,5))
+
+console.log("hello")
