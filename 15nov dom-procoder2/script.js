9a00e7b1db39457e505a3979efd5ac65eb858f8e
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-03-14 23:22:55 +0530

    Add files via upload

---
+++ 15nov dom-procoder2/script.js
@@ -0,0 +1,51 @@
+const allimages = document.getElementsByTagName('img')
+console.log(allimages);
+
+// and we also use document.images
+
+// const all = document.images
+// so we can use document.images for see all images in page.
+
+// const h2 = document.getElementsByTagName('h2')[0].innerText
+// console.log(h2);
+
+// const img = document.getElementsByClassName('css-image')[0]
+// console.log(img);
+
+// const im = document.getElementById('css-img')
+
+// id must be unque for each element so when we use getelementbyid then it will return the first value 
+// which it will find and return it.
+// we can give classes same name but not id.
+
+// let a = document.querySelector('.css-img')
+
+// let b = document.querySelectorAll('#css-img')
+
+// let c = document.querySelector('.img2')
+
+// let d = document.querySelector('[alt ="javascript roadmap"]')
+
+// let e = document.querySelector('ul li')
+
+// let f = document.querySelector('[alt ="javascript roadmap"]')
+// f.src = 'https://img.freepik.com/premium-photo/wide-angle-shot-single-tree-growing-clouded-sky-sunset-surrounded-by-grass_181624-22807.jpg?semt=ais_hybrid'
+
+// let allim = document.querySelectorAll('img')
+
+// let allimgarr = [
+//     'https://i0.wp.com/picjumbo.com/wp-content/uploads/magical-spring-forest-scenery-during-morning-breeze-free-photo.jpg?w=600&quality=80',
+//     'https://t4.ftcdn.net/jpg/07/59/06/79/360_F_759067997_vlz99eotxqkOGynGRtJVksPZCVW5OpbH.jpg',
+//     'https://static.vecteezy.com/system/resources/thumbnails/050/810/805/small/a-wooden-table-with-a-view-of-a-lush-green-forest-the-sunlight-is-shining-through-the-trees-creating-a-serene-and-peaceful-atmosphere-photo.jpg',
+
+// ]
+
+// for(let i = 0; i<allim.length;i++){
+//     allim[i].src = allimgarr[i];
+// }
+
+// allim.forEach((n,i)=>{
+//     n.src = allimgarr[i];
+    
+
+// })
