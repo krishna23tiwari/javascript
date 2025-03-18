264b4652976eb067b7d68310d628bf52852036bf
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-03-18 18:49:47 +0530

    Add files via upload

---
+++ 20nov class-prac/script.js
@@ -0,0 +1,28 @@
+// let a = ["car", "madam", "racecar"]
+
+// let b = a.find((n) => n === n.split('').reverse().join(''));
+// console.log(b);
+
+
+// let  a = [1,33,55,66,33,68,32]
+
+// let b = a.reduce((n,a1) => a1>n ? a1:n)
+// console.log(b);
+
+// let  a = [1,33,55,[66,33],68,32]
+
+// let b = a.reduce((n,a1) => n.concat(a1), []);
+// console.log(b);
+
+let  a = 
+   [22,55,1,2,4,5,88]
+
+
+let b = a.reduce((n,a1) => n%2 == 0? a1+n: n);
+console.log(b);
+
+
+
+
+
+
