fec53c819ab3bb2e51398e16d787470a6c74f2a6
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-03-24 19:28:35 +0530

    Add files via upload

---
+++ 15nov dom-procoder/script.js
@@ -0,0 +1,15 @@
+// document.body.children[5].children[2].children[4].src = 'https://cdn.pixabay.com/photo/2023/09/22/06/44/lion-8268242_640.jpg'
+// document.body.children[5].children[2]
+
+// document.body.children[5].children[2].innerText
+
+// const a = document.body.children[6]
+// console.log(a)
+// a.innerText
+
+// console.dir(document.body.children[4])
+
+// document.body.childNodes
+
+// //inner html shows inner html with text but text only shows inner text
+// document.body.children[0].innerHTML = 'krishna'
