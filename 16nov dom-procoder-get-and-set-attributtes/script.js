8a71a676b3c33f7730143a8f31e01123a679d684
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-04-29 18:22:13 +0530

    Add files via upload

---
+++ 16nov dom-procoder-get-and-set-attributtes/script.js
@@ -0,0 +1,8 @@
+// document.querySelector('[pro = "heloo"]').getAttribute('pro')
+
+// document.querySelector('[pro = "heloo"]').attributes
+
+// document.querySelector('h1')
+// document.querySelector('h1').getAttribute('class')
+
+// document.querySelector('h1').setAttribute('class','hi')
