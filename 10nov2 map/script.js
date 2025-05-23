444dcb8065b2fe501e746f17a8e19591d548e7ae
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-05-23 17:07:14 +0530

    Add files via upload

--- 10nov2 map/script.js
+++ 10nov2 map/script.js
@@ -1,10 +1,35 @@
-const a  = [12,33,45,11,44,22,44,55,66]
+// const a  = [12,33,45,11,44,22,44,55,66]
 
-// const b = a.map((n) => n + 10)
-// console.log(b);
+// // const b = a.map((n) => n + 10)
+// // console.log(b);
 
-// let x = a.map((n) => n - 10).filter((n) => n > 30).reduce((n,m) => n+ m)
+// // let x = a.map((n) => n - 10).filter((n) => n > 30).reduce((n,m) => n+ m)
 
-let x = a.map((n) => n - 10).filter((n) => n > 30).findIndex((n) => n ===34)
+// let x = a.map((n) => n - 10).filter((n) => n > 30).findIndex((n) => n ===34)
 
-console.log(x);
+// console.log(x);
+
+// let arr = ['kk', 'hulk', 'dev']
+// console.log(arr)
+
+
+console.log('a')
+console.log('b')
+
+console.log('c')
+setTimeout(() => {
+    console.log('d')
+}, 1000);
+
+setTimeout(() => {
+    console.log('e')
+}, 5000);
+
+setTimeout(() => {
+    console.log('f')
+}, 2000);
+
+console.log('g')
+console.log('h')
+
+
