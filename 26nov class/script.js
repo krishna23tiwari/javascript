2e1fac73aac44c9aaeffbc0af177b5e249a49ab3
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2025-07-07 19:09:54 +0530

    Add files via upload

---
+++ 26nov class/script.js
@@ -0,0 +1,30 @@
+// let a = [1,2,3,4,5,6]
+// let b = [4,5,6,7,8,9]
+
+// let xx = a.reduce((acc,n) =>{
+//     if(b.includes(n)) acc.push(n)
+//         return acc;
+// },[])
+
+// console.log(xx)
+
+// let a = [1,2,3,4,5,6]
+// let b = [4,5,6,7,8,9]
+
+// let x1 = a.reduce(((acc, n) => 
+//     b.includes(n)? (acc.push(n),acc) : acc),
+//     []
+// )
+
+// console.log(x1)
+
+// let a = [
+//     [1,2,3],
+//     [4,11,12],
+//     [7,8,9],
+// ];
+
+// let aa = a.reduce((acc,x) =>(acc.push(...x), acc),[]);
+// console.log(aa)
+
+let x2 = ['apple', ]
